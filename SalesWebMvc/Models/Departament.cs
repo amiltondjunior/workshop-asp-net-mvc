e9b515deefcc12f6df14ad57043fa48839f95b06
Amilton Junior <amiltongomes@hotmail.com>
2019-11-12 10:42:46 -0300

    MYSQL adaptation and first migration

--- SalesWebMvc/Models/Departament.cs
+++ SalesWebMvc/Models/Departament.cs
@@ -10,6 +10,7 @@ namespace SalesWebMvc.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Descricao { get; set; }
+        //public string Rating { get; set; }
 
     }
 }
